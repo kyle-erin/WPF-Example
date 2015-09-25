c48121d3ae77619d930f06b99f987b8a5b44380e
kyle phillips <spaznur@gmail.com>
2015-09-24 20:30:05 -0700

    Added window for "pretty printing" user data

---
+++ WPF-Example/WPF-Example/DisplayWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Example
+{
+    /// <summary>
+    /// Interaction logic for DisplayWindow.xaml
+    /// </summary>
+    public partial class DisplayWindow : Window
+    {
+        public DisplayWindow(string name, string email, string gender)
+        {
+            InitializeComponent();
+            lblName.Content = name;
+            lblEmail.Content = email;
+            lblGender.Content = gender;
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
